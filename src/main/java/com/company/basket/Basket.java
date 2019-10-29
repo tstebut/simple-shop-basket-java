b82f3ef523113f0eead09464ad74b0237dd7c54a
Ass7 <tanguy.von.stebut@gmail.com>
2019-10-29 14:20:15 +0000

    Quick implementation of the Discount feature :/

--- src/main/java/com/company/basket/Basket.java
+++ src/main/java/com/company/basket/Basket.java
@@ -1,12 +1,19 @@
 package com.company.basket;
 
 import java.util.*;
+import com.company.item.*;
+import com.company.basket.discount.*;
 import java.text.DecimalFormat;
 
 public class Basket<T extends Item> {
 
     private Map<T, Integer> items = new HashMap<>();
+    private Map<T, Discount> possibleDiscounts = new HashMap<>();
 
+    public void applyDiscounts(Map<T, Discount> discounts){
+        this.possibleDiscounts = discounts;    
+    }
+    
     public void addItemToBasket(T item, int quantity) {
         Integer oldValue = items.putIfAbsent(item, quantity);
         if (oldValue != null)
@@ -22,9 +29,12 @@ public class Basket<T extends Item> {
 
     public double totalBasketPrice() {
         List<Double> priceList = new ArrayList<>();
-
         for (Map.Entry<T, Integer> entry : this.items.entrySet()) {
-            priceList.add(entry.getValue() * entry.getKey().getPrice());
+            if (possibleDiscounts.containsKey(entry.getKey()))
+                priceList.add(possibleDiscounts.get(entry.getKey())
+                    .getDiscountPrice(entry.getKey(), entry.getValue()));
+            else
+                priceList.add(entry.getValue() * entry.getKey().getPrice());
         }
         DecimalFormat newFormat = new DecimalFormat("#.##");
         return Double.valueOf(newFormat.format(
@@ -36,7 +46,7 @@ public class Basket<T extends Item> {
     }
 
 
-    void showBasket() {
+    public void showBasket() {
         for (Map.Entry<T, Integer> entry : this.items.entrySet()) {
             System.out.println("Item: " + entry.getKey().getName() + ", quantity: " + entry.getValue() + ", price: " + entry.getKey().getPrice() + "£");
         }
